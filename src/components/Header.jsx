8f0b46e6fc0954e67b51779ffa3ba7eca52a17ab
Diva <150629389+Paprika3081@users.noreply.github.com>
2025-04-15 14:54:04 +1100

    react18

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
+// src/components/Header.jsx
 import React from "react";
 import { Button, Typography } from "antd";
 
 const { Title } = Typography;
 
-const Header = ({ onGetUsers }) => {
+const Header = ({ onGetUsers, onReset }) => {
   return (
-    <div style={{ padding: "20px", backgroundColor: "#f0f2f5" }}>
-      <Title level={2}>Получение АПИ с фильтрацией</Title>
-      <Button onClick={onGetUsers} type="primary" style={{ marginRight: 10 }}>
-        Получить пользователей
-      </Button>
+    <div className="controls">
+      <div className="title">
+        <Title className="title1">Получение АПИ с фильтрацией</Title>
+      </div>
+      <div className="buttons">
+        <Button onClick={onGetUsers} type="primary">
+          Получить пользователей
+        </Button>
+      </div>
     </div>
   );
 };
